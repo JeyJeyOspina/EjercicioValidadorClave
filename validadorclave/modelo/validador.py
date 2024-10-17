e332224a3023f4c81a5a31f9256e28840fa9d1f2
Juan José <juanjoos1905@gmail.com>
2024-10-17 12:10:34 -0500

    Create the __init__ of Abstract class ReglaValidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,16 @@
-# TODO: Implementa el código del ejercicio aquí
+from validadorclave.modelo.errores import ValidadorError
+from abc import  ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, _longitud_esperada: int):
+        self._longitud_esperada: int = _longitud_esperada
+
+    @abstractmethod
+    def es_valida(self):
+        pass
+
+
+
+
