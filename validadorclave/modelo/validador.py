2c84468a0c4ed32796e62ea99652ee0248ba3436
Juan José <juanjoos1905@gmail.com>
2024-10-17 12:21:26 -0500

    Se realiza la lógica de los metódos validar_longitud y contiene_mayuscula

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -12,10 +12,16 @@ class ReglaValidacion(ABC):
         pass
 
     def _validar_longitud(self, clave: str) -> bool:
-        ...
+        if len(clave) > self._longitud_esperada:
+            return True
+        else:
+            return False
 
     def _contiene_mayuscula(self, clave: str) -> bool:
-        ...
+        for caracter in clave:
+            if caracter.isupper():
+                return True
+        return False
 
     def _contiene_minuscula(self, clave: str) -> bool:
         ...
